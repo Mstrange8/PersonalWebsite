693180bd86f372d55f462beaa4cfb9bc2b8195b1
Mstrange8 <matthewsstrange@gmail.com>
2020-10-17 08:31:14 -0700

    Update settings.py

--- website/website/settings.py
+++ website/website/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -25,7 +25,7 @@ SECRET_KEY = '44!yghmmn8h=4fww^9ixg1nyn-^-n$(b*4z20=0_mah&8d(=&6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
